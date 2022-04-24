1663edba1053744851c36201041f02ba4485c33a
CollinClifford <collinclifford@ymail.com>
2022-04-24 10:52:53 -0700

    changed-url

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/", router);
+app.use("/data", router);
 
 app.use(notFound);
 app.use(errorHandler);
--- src/db/migrations/20220421060531_createToDoTable.js
+++ src/db/migrations/20220421060531_createToDoTable.js
@@ -9,14 +9,13 @@ exports.up = function (knex) {
     table.string("description");
     table.string("due_date");
     table.string("tags");
-    table.timestamps(true, true); // adds created_at and updated_at columns; passing true as the first argument sets the columns to be a timestamp type while passing true as the second argument sets those columns to be non-nullable and to use the current timestamp by default
   });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
- */
+ */ 
 exports.down = function (knex) {
   return knex.schema.dropTable("todos");
 };
